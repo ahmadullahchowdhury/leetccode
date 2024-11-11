6fdeab7b5ba0df2d1a2311f31aec16ec566a3745
ahmadullachowdhury <kowcher99@gmail.com>
2024-11-11 11:12:07 +0600

    stock sell dp

---
+++ 121. Best Time to Buy and Sell Stock/js.js
@@ -0,0 +1,16 @@
+
+
+var maxProfit = function (prices) {
+    let minPrice = prices[0];
+    let maxProfit = 0;
+    for(let price of prices){
+        // currentProfit = price - minPrice;
+        maxProfit = Math.max(maxProfit, price - minPrice);
+        minPrice = Math.min(price, minPrice);
+    }
+    return maxProfit
+};
+
+const prices = [7,1,5,3,6,4];
+
+console.log(maxProfit(prices));
