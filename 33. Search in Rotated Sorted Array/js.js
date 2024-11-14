0214dc60702d4cb0e88b32c77c94b739ae43ee62
ahmadullachowdhury <kowcher99@gmail.com>
2024-11-14 13:26:08 +0600

    33. Search in Rotated Sorted Array

---
+++ 33. Search in Rotated Sorted Array/js.js
@@ -0,0 +1,32 @@
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        }
+        
+        //left sorted portion
+        if (nums[left] <= nums[mid] )  {
+            if ( target >= nums[left]  &&  target < nums[mid]){
+                right = mid -1;
+            } else {
+                left = mid + 1
+            }
+        }
+        
+        //right sorted portion
+        else{
+            if( target > nums[mid] && target <= nums[right] ){
+                left = mid + 1;
+            } else{
+                right = mid - 1;
+            }
+        }
+    }
+    return -1;
+};
+
+console.log(search([4,5,6,7,0,1,2], 0));
